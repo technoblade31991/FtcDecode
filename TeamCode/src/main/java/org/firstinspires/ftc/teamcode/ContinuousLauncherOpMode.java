83fe254572d77141bb9d066b00add74710269b70
Vihaan Krishnan <krisvih32@yahoo.com>
2025-09-30 18:39:37 -0700

    Added continuous launcher op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ContinuousLauncherOpMode.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name = "ContinuousLauncherOpMode")
+public class ContinuousLauncherOpMode extends LinearOpMode {
+    private CRServo left_feeder;
+    private CRServo right_feeder;
+    private DcMotor launcher;
+    @Override
+    public void runOpMode() {
+        left_feeder = hardwareMap.crservo.get("left_feeder");
+        right_feeder = hardwareMap.crservo.get("right_feeder");
+        launcher = hardwareMap.dcMotor.get("launcher");
+        left_feeder.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        while (opModeIsActive()) {
+            launcher.setPower(0.6);
+            sleep(3000);
+            left_feeder.setPower(1);
+            right_feeder.setPower(1);
+        }
+    }
+}
