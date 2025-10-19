42d43df3a1c1b0b6eb4fe4f95a422bf800926708
Vihaan Krishnan <krisvih32@yahoo.com>
2025-10-19 16:11:44 -0700

    WIP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomousOpMode.java
@@ -10,27 +10,21 @@ import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
 public class MainAutonomousOpMode extends LinearOpMode {
 
 
-    private static final double DRIVE_START_FORWARD_MS = 10_000;
+    private static final long DRIVE_START_FORWARD_MS = 10_000;
 
 
     @Override
     public void runOpMode(){
         // Initialize hardware
 
-        // Initialize apriltag
-        AprilTag aprilTag = new AprilTag();
-        aprilTag.init(hardwareMap);
-
         // Initialize mecanum drive
         MecanumDrive drive = new MecanumDrive();
         drive.init(hardwareMap);
 
-        double angleToTurn = 0;
-        waitForStart();
-        while (opModeIsActive()) {
-            aprilTag.addTelemetry(telemetry);
-            angleToTurn = -aprilTag.pose.bearing;
-            drive.driveRelativeRobot(angleToTurn, 0, 0);
-        }
+        // Drive forward for DRIVE_START_FORWARD_MS
+
+        drive.driveRelativeField(1, 0, 0);
+        sleep(DRIVE_START_FORWARD_MS);
+        drive.driveRelativeField(0, 0, 0);
     }
 }
