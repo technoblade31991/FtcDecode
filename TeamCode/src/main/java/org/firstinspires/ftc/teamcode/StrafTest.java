df2f68e380c73e78d7602181c74d11219d389834
Abhirama Valluri <abhiramavalluri@gmail.com>
2025-11-11 17:54:17 -0800

    disable other op-modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StrafTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StrafTest.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@Disabled
 @TeleOp(name="StrafeLeft6Inches", group="Straf")
 public class StrafTest extends LinearOpMode {
 
